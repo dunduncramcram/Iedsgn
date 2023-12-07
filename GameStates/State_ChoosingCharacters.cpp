ec01b3733804efc74b6a84e12d3b3743343159ea
dunduncramcram <153187752+dunduncramcram@users.noreply.github.com>
2023-12-07 19:47:32 +0800

    Update State_ChoosingCharacters.cpp

--- GameStates/State_ChoosingCharacters.cpp
+++ GameStates/State_ChoosingCharacters.cpp
@@ -1,6 +1,13 @@
 
 #include "StatesHeader.hpp"
-#include <iostream>
+#include<iostream>
+#include<stdlib.h> /*system("CLS");*/
+#include<conio.h> /*_getch();*/
+#include<vector>
+
+#include"Characters.hpp"
+
+using namespace std;
 
 enum State_ChoosingCharacters::stateOptions : unsigned int {
     
@@ -13,7 +20,62 @@ void State_ChoosingCharacters::stateLogic(GameController* game){
 state_beginning:
     stateRender(game);
 
-    std::cout << "Need to iterate character choosing between patients based on patient count, then pushing to the patient vector in the game" << std::endl;
+    vector<Character> charac;
+
+Ability ability = new Action_DebtCollectorsAbility(*(game->getCurrentState()));
+Character character = new Character("Debt Collector", &ability);
+charac->push_back(character);
+
+ability = new Action_FathersAbility(*(game->getCurrentState()));
+character = new Character("Father", &ability);
+charac->push_back(character);
+
+ability = new Action_JournalistsAbility(*(game->getCurrentState()));
+character = new Character("Journalist", &ability);
+charac->push_back(character);
+
+ability = new Action_PastorsAbility(*(game->getCurrentState()));
+character = new Character("Pastor", &ability);
+charac->push_back(character);
+
+ability = new Action_StudentsAbility(*(game->getCurrentState()));
+character = new Character("Student", &ability);
+charac->push_back(character);
+
+ability = new Action_VeteransAbility(*(game->getCurrentState()));
+character = new Character("Veteran", &ability);
+charac->push_back(character);
+
+char input;
+string name;
+int i, j = -1;
+unsigned int numPatient = game->getPatientCount();
+
+for (i = 0; i < numPatient; i++) {
+	int numCharacters = charac->getsize();
+
+	cout << "What's your name?"; cin >> name;
+	cout << endl;
+
+	do {
+		if (j = numCharacters - 1) j = 0;
+		else j++;
+
+		system("CLS");
+
+		cout << endl << "Player " << i + 1 << endl;
+		cout << "Play as this character?" << endl;
+		cout << "(Press q to select, otherwise press any key)" << endl << endl;
+		cout << "Character: " << charac[j]->getName() << endl;
+		input = _getch();
+
+	} while (input != 'q' && input != 'Q');
+
+	Patient patient = new Patient(name, characters[j]);
+	patients->push_back(&patient);
+
+	charac->erase(j);
+}
     
     game->setState(new State_DealingCards());
 };
