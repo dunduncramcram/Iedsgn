b73d951fd6d987c86dc133c9da4138832c20d8cc
TooMuchKofii <151616808+DevKofii@users.noreply.github.com>
2023-12-07 18:40:02 +0800

    [StatesRenderDefs.cpp] Overhaul text renders
    Flourished main texts to ascii art. Should be presentable enough i guess

--- GameStates/StatesRenderDefs.cpp
+++ GameStates/StatesRenderDefs.cpp
@@ -5,77 +5,209 @@
 using namespace std;
 
 void State_MainMenu::stateRender(GameController* game){
-    cout 
-        << "Main Menu:" << endl
-        << "[1] Start a game" << endl 
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __" << endl
+        << " _                           " << endl
+        << "|_)  _     _ |_   _   _ o  _ " << endl
+        << "|   _> \/ (_ | | (_) _> | _> " << endl
+        << "       /                     " << endl  
+        << "__ __ __ __ __ __ __ __ __ __" << endl << endl
+        << "[1] Start a game" << endl
         << "[2] See Instructions" << endl
-        << "[3] Exit" << endl 
+        << "[3] Exit" << endl
         << "Enter your choice: "
-    ;
+        ;
 };
 
 void State_ShowingInstructions::stateRender(GameController* game){
-    cout << "Showing Instructions:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << "___                                       " << endl
+        << " |  ._   _ _|_ ._      _ _|_ o  _  ._   _ " << endl
+        << "_|_ | | _>  |_ |  |_| (_  |_ | (_) | | _> " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Showing Instructions:" << endl;
 };
 
 void State_ShowingDetailedInstructions::stateRender(GameController* game){
-    cout << "Showing Detailed Instructions:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                            ___                                       " << endl
+        << "| \  _  _|_  _. o |  _   _|    |  ._   _ _|_ ._      _ _|_ o  _  ._   _ " << endl
+        << "|_/ (/_  |_ (_| | | (/_ (_|   _|_ | | _>  |_ |  |_| (_  |_ | (_) | | _> " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Showing Detailed Instructions:" << endl;
 };
 
 void State_ExitingApp::stateRender(GameController* game){
-    cout << "Exiting Application..." << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                                                            " << endl
+        << "|_    o _|_ o ._   _     /\  ._  ._  | o  _  _. _|_ o  _  ._  " << endl
+        << "|_ >< |  |_ | | | (_|   /--\ |_) |_) | | (_ (_|  |_ | (_) | | " << endl
+        << "                   _|        |   |                            " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Exiting Application..." << endl;
 };
 
 void State_NamingDirector::stateRender(GameController* game){
-    cout << "Naming Director:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << "                            _                         " << endl
+        << "|\ |  _. ._ _  o ._   _    | \ o ._  _   _ _|_  _  ._ " << endl
+        << "| \| (_| | | | | | | (_|   |_/ | |  (/_ (_  |_ (_) |  " << endl
+        << "                      _|                              " << endl           
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Naming Director:" << endl;
 };
 
 void State_SummoningMonster::stateRender(GameController* game){
-    cout << "Summoning Monster:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " __                                                                " << endl
+        << "(_      ._ _  ._ _   _  ._  o ._   _    |\/|  _  ._   _ _|_  _  ._ " << endl
+        << "__) |_| | | | | | | (_) | | | | | (_|   |  | (_) | | _>  |_ (/_ |  " << endl
+        << "                                   _|                              " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Summoning Monster:" << endl;
 
 };
 
 void State_CountingPlayers::stateRender(GameController* game){
-    cout << "Counting Players:" << endl;
-
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << "_                              _" << endl
+        << "/   _      ._  _|_ o ._   _    |_) |  _.     _  ._  _ " << endl
+        << "\_ (_) |_| | |  |_ | | | (_|   |   | (_| \/ (/_ |  _> " << endl
+        << "                          _|             /            " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Counting Players:" << endl;
 };
 
 void State_ChoosingCharacters::stateRender(GameController* game){
-    cout << "Choosing Characters:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                             _                                 " << endl
+        << "/  |_   _   _   _ o ._   _    /  |_   _. ._  _.  _ _|_  _  ._  _ " << endl
+        << "\_ | | (_) (_) _> | | | (_|   \_ | | (_| |  (_| (_  |_ (/_ |  _> " << endl
+        << "                         _|                                      " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Choosing Characters:" << endl;
 
 };
 
 void State_DealingCards::stateRender(GameController* game){
-    cout << "Dealing Cards:" << endl;
-
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                         _               " << endl
+        << "| \  _   _. | o ._   _    /   _. ._  _|  _ " << endl
+        << "|_/ (/_ (_| | | | | (_|   \_ (_| |  (_| _> " << endl
+        << "                     _|                    " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Dealing Cards:" << endl;
 };
 
 void State_PatientTurn::stateRender(GameController* game){
-    cout << "Patient's Turn:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                               ___            " << endl
+        << "|_)  _. _|_ o  _  ._  _|_ /  _    |      ._ ._  " << endl
+        << "|   (_|  |_ | (/_ | |  |_   _>    |  |_| |  | | " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Patient's Turn:" << endl;
 
 };
 
 void State_ResolvingDilemmas::stateRender(GameController* game){
-    cout << "Resolving Dilemmas: " << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                               _                             " << endl
+        << "|_)  _   _  _  |    o ._   _    | \ o |  _  ._ _  ._ _   _.  _ " << endl
+        << "| \ (/_ _> (_) | \/ | | | (_|   |_/ | | (/_ | | | | | | (_| _> " << endl
+        << "                           _|                                  " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Resolving Dilemmas: " << endl;
 
 };
 
 void State_DirectorTurn::stateRender(GameController* game){
-    cout << "Director's Turn:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _                                ___            " << endl
+        << "| \ o ._  _   _ _|_  _  ._ /  _    |      ._ ._  " << endl
+        << "|_/ | |  (/_ (_  |_ (_) |    _>    |  |_| |  | | " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Director's Turn:" << endl;
 };
 
 void State_HungerPangs::stateRender(GameController* game){
-    cout << "Hunger Pangs:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << "                          _                 " << endl
+        << "|_|     ._   _   _  ._   |_)  _. ._   _   _ " << endl
+        << "| | |_| | | (_| (/_ |    |   (_| | | (_| _> " << endl
+        << "             _|                       _|    " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Hunger Pangs:" << endl;
 };
 
 void State_RoundEnd::stateRender(GameController* game){
-    cout << "End Of Round:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " _            _    _    _                  " << endl
+        << "|_ ._   _|   / \ _|_   |_)  _      ._   _| " << endl
+        << "|_ | | (_|   \_/  |    | \ (_) |_| | | (_| " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "End Of Round:" << endl;
 };
 
 void State_GameOver::stateRender(GameController* game){
-    cout << "Game Over" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << " __                  _            " << endl
+        << "/__  _. ._ _   _    / \     _  ._ " << endl
+        << "\_| (_| | | | (/_   \_/ \/ (/_ |  " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Game Over" << endl;
 };
 
 void State_AskReplay::stateRender(GameController* game){
-    cout << "Ask to Replay:" << endl;
+    system("cls");
+    cout << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << "             ___        _                   " << endl
+        << " /\   _ |     |   _    |_)  _  ._  |  _.    " << endl
+        << "/--\ _> |<    |  (_)   | \ (/_ |_) | (_| \/ " << endl
+        << "                               |         /  " << endl
+        << "__ __ __ __ __ __ __ __ __ __ __ __ __ __ __" << endl
+        << endl
+        ;
+    //cout << "Ask to Replay:" << endl;
 };
